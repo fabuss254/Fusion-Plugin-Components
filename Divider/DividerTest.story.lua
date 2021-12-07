bf19940f1419715773839a7a0a549993b9b770be
fabuss254 <26258237+fabuss254@users.noreply.github.com>
2021-12-07 13:22:53 +0100

    Update DividerTest.story.lua

--- Divider/DividerTest.story.lua
+++ Divider/DividerTest.story.lua
@@ -11,14 +11,25 @@ return function(p)
 	if ss then ss.ZIndexBehavior = Enum.ZIndexBehavior.Global end
 	
 	local UI = New "Frame" {
-		Size = UDim2.new(1, -1, 1, -1),
+		Size = UDim2.new(1, -1, 1, -1), -- Our main frame have 1 pixel less of size due to how borders work.
 		
 		[Children] = Divider(
 			"Y",
 			{UDim.new(0, 20)},
 			{UDim.new(1, -40), Divider(
 				"X",
-				{UDim.new(0.5, 0)},
+				{UDim.new(0.1, 0), Divider(
+					"Y",
+					{UDim.new(0.1, 0)},
+					{UDim.new(0.1, 0)},
+					{UDim.new(0.1, 0)},
+					{UDim.new(0.1, 0)},
+					{UDim.new(0.1, 0)},
+					{UDim.new(0.2, 0)},
+					{UDim.new(0.2, 0)},
+					{UDim.new(0.1, 0)}
+				)},
+				{UDim.new(0.4, 0)},
 				{UDim.new(0.5, 0)}
 			)},
 			{UDim.new(0, 20)}
